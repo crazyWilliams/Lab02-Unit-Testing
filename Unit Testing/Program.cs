19f7ceb681fdc12792cc9315102999cd3abce633
Williams Argenal <williamsargenal@outlook.com>
2019-06-12 19:27:08 -0700

    trying to get my lab up in general

---
+++ Unit Testing/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Unit_Testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Welcome to Seattle Bank's ATM");
+                // 
+                //  AtmWoke();
+            }
+
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+
+            }
+
+           
+
+
+        }
+
+       static void ATMWoke()
+        {
+            try
+            {
+
+
+                Console.WriteLine("Please select one of the following:");
+                Console.WriteLine("1 - View Balance");
+                Console.WriteLine("2 - Withdraw Money");
+                Console.WriteLine("3 - Deposit Money");
+                Console.WriteLine("4 - Exit ");
+                decimal Balance = 4000;
+
+                // still need to fix things //
+               // static void viewBalance()
+                {
+
+                }
+            }
+
+            catch (Exception e)
+            {
+
+
+            }
+        }
+       
+
+
+
+    }
+}
